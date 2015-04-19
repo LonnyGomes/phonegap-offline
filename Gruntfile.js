3c9059ffe6f0bbf79a296d2332e7112f463a1837
Lonny Gomes <code@lonnygomes.com>
2015-04-19 18:34:56 -0400

    Incorporated grunt-bump task into the workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,24 @@ module.exports = function (grunt) {
                 }
             }
         },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        },
 
         // Unit tests.
         nodeunit: {
